fc967e35ac9a6e1211042145e21f20d9d3cdaac4
pti-geek-demo (Nauczyciel) <pti-geek@proton.me>
2022-12-14 19:45:13 +0100

    Pliki
    Nowe pliki demo

---
+++ kody/test.py
@@ -0,0 +1,12 @@
+import sys
+
+dane = sys.implementation
+print(dane)
+
+print("Hej - mój pierwszy program")
+print("-----")
+# print("Jak masz na imię?")
+my_name = input("Jak masz na imię?")
+print("Hej", my_name, "Ja jestem Python")
+print("Hej " + my_name + " Ja jestem Python")
+print(f"Hej {my_name} Ja jestem Python")
